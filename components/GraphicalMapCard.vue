0082a1208127c0d18b930900c3d4fa860dfbf550
fuyu <fuyu.fluffy@gmail.com>
2020-11-11 23:13:44 +0900

    注釈を追加

--- components/GraphicalMapCard.vue
+++ components/GraphicalMapCard.vue
@@ -29,6 +29,15 @@
     </template>
     <!-- <ibaraki-map /> -->
     <div id="map" ref="map" class="osaka-map" />
+    <p :class="$style.note">
+      <a
+        href="https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N03-v2_4.html"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {{ $t('「国土数値情報（行政区域）」（国土交通省）を加工して作成') }}
+      </a>
+    </p>
   </data-view>
   <!-- </v-col> -->
 </template>
